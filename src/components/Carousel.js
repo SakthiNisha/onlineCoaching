a623ad35967316177f8a7017c34d6f5de9db3d6d
SakthiNish <sakthi04itech@gmail.com>
2024-08-08 13:49:48 -0400

    added slider images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,20 +6,21 @@ import './Carousel.css'; // Import the CSS file
 const TechCoachingCarousel = () => {
   const carouselItems = [
     {
-      src: process.env.PUBLIC_URL + '/images/slider_background.jpg',
-      title: 'Tech Coaching 1',
-      description: 'Learn the basics of programming with our expert coaches.'
+        src: process.env.PUBLIC_URL + '/images/slider_background.jpg',
+        title: 'Comprehensive Courses in Python, React, Node, MongoDB, and JavaScript',
+        description: 'Unlock your potential with our comprehensive courses in todays most sought-after technologies. Our curriculum is designed to provide a deep understanding of'
     },
     {
-      src: process.env.PUBLIC_URL + '/images/slider_background.jpg',
-      title: 'Tech Coaching 2',
-      description: 'Advanced programming techniques and best practices.'
+      src: process.env.PUBLIC_URL + '/images/interactive-live-online-classes.jpeg',
+      title: 'Interactive Live Classes',
+      description: 'Join our interactive live classes to learn Python, React, and Node from industry experts in real-time. Engage with instructors, ask questions, and collaborate with peers in a dynamic virtual classroom environment. Our live sessions provide a hands-on learning experience, ensuring you get the most out of your training. Enroll now and take the first step towards mastering the technologies that power the modern web.'
     },
     {
-      src: process.env.PUBLIC_URL + '/images/slider_background.jpg',
-      title: 'Tech Coaching 3',
-      description: 'Mastering data structures and algorithms.'
+      src: process.env.PUBLIC_URL + '/images/code-excercise-1.jpeg',
+      title: 'Engaging Interactive Coding Exercises',
+      description: 'Boost your learning with our engaging interactive coding exercises. Designed to provide hands-on experience, these exercises allow you to practice and apply concepts in real-time. Whether you are learning Python, React, or Node, our coding challenges will help you build confidence and proficiency. Immediate feedback and detailed solutions ensure you understand every step. Dive in and transform your coding skills through practical, interactive exercises tailored to your learning pace.'
     }
+    
   ];
 
   return (
@@ -32,7 +33,7 @@ const TechCoachingCarousel = () => {
             alt={item.title}
           />
           <Carousel.Caption>
-            <h3>{item.title}</h3>
+            <h3 className="carousel-title">{item.title}</h3>
             <p>{item.description}</p>
           </Carousel.Caption>
         </Carousel.Item>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
 // src/components/Header.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'; // Import the CSS file
 
 const Header = () => {
   return (
     <header className="fixed-top">
       <nav className="navbar navbar-expand-lg navbar-white bg-warning">
-        <Link className="navbar-brand header-brand-name" to="/">My Website</Link>
+        <NavLink className="navbar-brand header-brand-name" to="/">My Website</NavLink>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link className="nav-link header-link" to="/">Home</Link>
+              <NavLink className="nav-link header-link" to="/">Home</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link header-link" to="/courses">Courses</Link>
+              <NavLink className="nav-link header-link" to="/courses">Courses</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link header-link" to="/about-us">About Us</Link>
+              <NavLink className="nav-link header-link" to="/about-us">About Us</NavLink>
             </li>
             <li className="nav-item">
-              <Link className="nav-link header-link" to="/contact">Contact Us</Link>
+              <NavLink className="nav-link header-link" to="/contact">Contact Us</NavLink>
             </li>
           </ul>
         </div>
