d84cc0fc915c35c0d29e40a35bb70eca1a7c673b
SakthiNish <sakthi04itech@gmail.com>
2024-08-09 10:36:08 -0400

    header toggle menu fix for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
   return (
     <header className="fixed-top">
       <div className="contact-info bg-warning">
-        <span>Phone / WhatsAPP: +123 456 7890</span>
+        <span>Phone / WhatsApp: +123 456 7890</span>
       </div>
       <nav className="navbar navbar-expand-lg navbar-white bg-white">
         <NavLink className="navbar-brand header-brand-name" to="/">
           <img src={process.env.PUBLIC_URL + '/images/sparkle.jpg'} alt="Logo" className="header-logo" />
-          </NavLink>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        </NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
